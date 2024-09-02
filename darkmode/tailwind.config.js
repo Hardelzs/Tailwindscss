9f4b6ab0a16c96a2a497bfef061aa8430ef2119d
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2024-09-02 11:46:31 +0100

    npm build

--- darkmode/tailwind.config.js
+++ darkmode/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: [
+    './dark.html',
+    './index.html'
+  ],
   darkMode: 'class',
   theme: {
     screens: {
