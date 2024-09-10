6df8a1c55a478fe004060c78f3b6fa7688d6e15f
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2024-09-10 14:00:20 +0100

    sixth

--- darkmode/js/index.js
+++ darkmode/js/index.js
@@ -1,43 +1,61 @@
-const sunIcon = document.querySelector('.sun');
-const moonIcon = document.querySelector('.moon');
+// const sunIcon = document.querySelector('.sun');
+// const moonIcon = document.querySelector('.moon');
 
-const userTheme = localStorage.getItem('theme');
-const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+// const userTheme = localStorage.getItem('theme');
+// const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-const iconToggle = () => {
-    sunIcon.classList.toggle("display-none");
-    moonIcon.classList.toggle("display-none");
-};
+// const iconToggle = () => {
+//     sunIcon.classList.toggle("display-none");
+//     moonIcon.classList.toggle("display-none");
+// };
 
-const themeCheck = () => {
-    if (userTheme === "dark" || (!userTheme && systemTheme)) {
-        document.documentElement.classList.add("dark");
-        moonIcon.classList.add("display-none");
-        sunIcon.classList.remove("display-none");
-        return;
-    }
-    sunIcon.classList.add("display-none");
-    moonIcon.classList.remove("display-none");
-};
+// const themeCheck = () => {
+//     if (userTheme === "dark" || (!userTheme && systemTheme)) {
+//         document.documentElement.classList.add("dark");
+//         moonIcon.classList.add("display-none");
+//         sunIcon.classList.remove("display-none");
+//         return;
+//     }
+//     sunIcon.classList.add("display-none");
+//     moonIcon.classList.remove("display-none");
+// };
 
-const themeSwitch = () => {
-    if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-        iconToggle();
-        return;
-    }
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("theme", "dark");
-    iconToggle();
-};
+// const themeSwitch = () => {
+//     if (document.documentElement.classList.contains("dark")) {
+//         document.documentElement.classList.remove("dark");
+//         localStorage.setItem("theme", "light");
+//         iconToggle();
+//         return;
+//     }
+//     document.documentElement.classList.add("dark");
+//     localStorage.setItem("theme", "dark");
+//     iconToggle();
+// };
 
-sunIcon.addEventListener("click", () => {
-    themeSwitch();
-});
+// sunIcon.addEventListener("click", () => {
+//     themeSwitch();
+// });
 
-moonIcon.addEventListener("click", () => {
-    themeSwitch();
-});
+// moonIcon.addEventListener("click", () => {
+//     themeSwitch();
+// });
 
-themeCheck();
+// themeCheck();
+
+
+const hamburger = document.getElementById('hamburg')
+const cancel = document.getElementById('cancel')
+
+cancel.addEventListener('click', () => {
+    const hamburg = document.getElementById('hamburg')
+    const cancel = document.getElementById('cancel')
+    cancel.style.display = 'none'
+    hamburg.style.display = 'block'
+})
+
+hamburger.addEventListener('click', () => {
+    const hamburg = document.getElementById('hamburg')
+    const cancel = document.getElementById('cancel')
+    cancel.style.display = 'block'
+    hamburg.style.display = 'none'
+})
